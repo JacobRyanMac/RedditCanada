e8f9cd9251ddd25559f99741129f25cfe3538a90
Jacob MacDougall <jacobryanmacdougall@gmail.com>
2017-08-02 10:00:40 -0400

    added constants

--- test/process_body/pickle_train_test.py
+++ test/process_body/pickle_train_test.py
@@ -4,18 +4,23 @@ import pickle
 from nltk import FreqDist
 from nltk.tokenize import word_tokenize
 
+# labels:
+label1 = 'Housing'
+label2 = 'Internet'
+
 # split by n means len(n)/5 are in train and 1/5 is in test
 split = lambda x: - int(len(x) / 5)
-file_name = "set_housing_internet.pickle"
+file_name = "set_" + label1.lower() + "_" + label2.lower() + ".pickle"
+
 db = sqlite3.connect('../canada_subreddit_test.db')
 cur = db.cursor()
 cur.execute('''
 SELECT c.body, s.label
 FROM submissions as s, comments as c
 WHERE s.submission_id = c.submission_id
-AND (s.label = 'Housing'
-OR s.label = 'Internet');
-''')
+AND (s.label = ?
+OR s.label = ?);
+''', (label1,label2,))
 
 documents = [(word_tokenize(c[0]), c[1]) for c in cur]
 random.shuffle(documents)
