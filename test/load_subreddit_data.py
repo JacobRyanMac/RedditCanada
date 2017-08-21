f61fd5e9123bf9bc43b27b08b9fea0a5449edb53
Jacob MacDougall <jacobryanmacdougall@gmail.com>
2017-08-21 15:22:29 -0400

    changed to use objects

--- test/load_subreddit_data.py
+++ test/load_subreddit_data.py
@@ -1,18 +1,18 @@
 import praw
 import datetime, time
 import sqlite3 as sql
-from redditSQL import SQL_submission, SQL_comment
+
+from Database import Query
+from Query import SubmissionQuery, CommentQuery, UserQuery
 
 # Constants
-whichDatabase = r'canada_subreddit.db'
+whichDatabase = r'../canada_subreddit.db'
 whichSubreddit = 'canada'
-scoreMin = 50
 commentMin = 50
 commentLimit = 100
 commentThreshold = 0
-days_back = 2
+days_back = 3
 ext_to = datetime.datetime.now()
-#ext_to = datetime.datetime(2017, 6, 7, 12, 0, 0, 0)
 ext_from = ext_to - datetime.timedelta(days=days_back)
 
 # Connect to reddit
@@ -26,34 +26,44 @@ canada = reddit.subreddit(whichSubreddit)
 # Extract submisions
 allSubmissions = canada.submissions(int(time.mktime(ext_from.timetuple())),
                                     int(time.mktime(ext_to.timetuple())))
+
 # Load the data into SQLite
 for submis in allSubmissions:
-    db = sql.connect(whichDatabase)
-    # if submis.score >= scoreMin or submis.num_comments >= commentMin:
+    db = Query(whichDatabase)
+    db.connect()
     if submis.num_comments >= commentMin:
         # Enter submission:
-        cur_submis = SQL_submission(submis)
+        sq = SubmissionQuery(submis)
         try:
-            db.execute(cur_submis.make_submission_query(True))
-            if submis.author is not None:
-                db.execute(cur_submis.make_user_query(True))
+            db.execute(sq.insert())
         except sql.IntegrityError:
-            db.execute(cur_submis.make_submission_query(False))
-            if submis.author is not None:
-                db.execute(cur_submis.make_user_query(False))
-        db.commit()
+            db.execute(sq.update())
+        # Enter users:
+        uq = UserQuery(submis)
+        if uq.id is not None:
+            try:
+                db.execute(uq.insert())
+            except sql.IntegrityError:
+                db.execute(uq.update())
+            db.commit()
         print("ID:", submis.id, "is added. Now inserting comments...")
+
         # Enter comment forest
         submis.comments.replace_more(limit=commentLimit, threshold=commentThreshold)
         for c in submis.comments.list():
-            cur_comment = SQL_comment(c)
+            # Comment:
+            cq = CommentQuery(c)
             try:
-                db.execute(cur_comment.make_comment_query(True))
-                if c.author is not None:
-                    db.execute(cur_comment.make_user_query(True))
+                db.execute(cq.insert())
             except sql.IntegrityError as e:
-                db.execute(cur_comment.make_comment_query(False))
-                if c.author is not None:
-                    db.execute(cur_comment.make_user_query(False))
+                db.execute(cq.update())
+            # User:
+            uq = UserQuery(c)
+            if uq.id is not None:
+                try:
+                    db.execute(uq.insert())
+                except sql.IntegrityError as e:
+                    db.execute(uq.update())
             db.commit()
+
     db.close()
